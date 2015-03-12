783799c27480600cc642d356a0a2517148b54842
lüftreich <luftreich@gmail.com>
2015-03-12 18:12:50 +0800

    Set SUID

--- Android.mk
+++ Android.mk
@@ -12,4 +12,22 @@ LOCAL_MODULE_PATH := $(TARGET_OUT_OPTIONAL_EXECUTABLES)
 LOCAL_MODULE_TAGS := eng debug
 LOCAL_FORCE_STATIC_EXECUTABLE := true
 
+
+SU_INSTALL_DIR := $(TARGET_OUT)/xbin
+SU_BINARY := $(SU_INSTALL_DIR)/su
+# taken from busybox-android
+$(SU_BINARY)-post: su
+	@echo "Setting SUID/GUID to su-binary..."
+	chmod ug+s $(TARGET_OUT_OPTIONAL_EXECUTABLES)/su
+
+SU_CMD := su
+SYMLINKS := $(addprefix $(TARGET_OUT_EXECUTABLES)/,$(SU_CMD))
+$(SYMLINKS): $(LOCAL_INSTALLED_MODULE) $(SU_BINARY)-post $(LOCAL_PATH)/Android.mk
+	@echo "Symlink: $@ -> /system/xbin/$(SU_CMD)"
+	@mkdir -p $(dir $@)
+	@rm -rf $@
+	@ln -sf /system/xbin/$(SU_CMD) $@
+
+ALL_DEFAULT_INSTALLED_MODULES += $(SU_BINARY)-post $(SYMLINKS)
+
 include $(BUILD_EXECUTABLE)
